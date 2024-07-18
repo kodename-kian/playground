0547e45df2b418bab5c9513700f1eb0111c31127
kodename-kian <kodename.kian@gmail.com>
2024-07-18 22:50:29 +0800

    firebase works, proper commit

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -5,15 +5,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 //Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_APP_ID
-// };
-
 const firebaseConfig = {
     apiKey: "AIzaSyBrBrk__P3pmvjp6yYsqzF2pK5xKi19UcU",
     authDomain: "playground-df0b8.firebaseapp.com",
